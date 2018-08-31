c9db72752b663bcef04cff43d94c2aeebeb27ae5
scliff6262 <sclifford6262@gmail.com>
2018-08-31 14:18:25 -0400

    altered NavBar and App components for new components

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
-  return <h1> NavBar here </h1>
+  return (
+    <div>
+      <NavLink to="/home">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="products">Products</NavLink>
+      <NavLink to="/contact">Contact Us</NavLink>
+    </div>
+  )
 }
 
 export default NavBar
--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from '../../components/NavBar/NavBar'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from '../Home/Home'
+import About from '../../components/About/About'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <Router>
+          <div>
+            <NavBar />
+            <Switch>
+              <Route exact path="/home" component={Home}/>
+              <Route exact path="/about" component={About}/>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
