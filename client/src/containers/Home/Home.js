c42bad8c26b9f9b5a4f6cac711a9feda21d9be10
scliff6262 <sclifford6262@gmail.com>
2018-08-31 14:17:54 -0400

    created Home container

---
+++ client/src/containers/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  constructor(){
+    super()
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Welcome to Lorem Ipsum store</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
